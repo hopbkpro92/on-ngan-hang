de4528c37a53bc17fd7e1876363d3e009b5a260c
Quang Hop Dang <hopbkpro92@gmail.com>
2025-05-08 00:40:05 +0700

    [bugfix] Modify enviroment variable

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,28 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_APP_URL: process.env.NODE_ENV === 'production' 
-      ? 'https://your-production-url.com' // Replace with your actual production URL
-      : 'http://localhost:9002', // Default for local development
-  }
+    /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+    env: {
+        NEXT_PUBLIC_APP_URL: process.env.NODE_ENV === 'production'
+            ? 'https://on-ngan-hang.vercel.app' // Replace with your actual production URL
+            : 'http://localhost:9002', // Default for local development
+    }
 };
 
 export default nextConfig;
